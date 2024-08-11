57ebb480e2d9bdee1aac56e1c5d2e299b4d54705
Rohith Gajawada <141448174+RohithGajawada45@users.noreply.github.com>
2024-08-11 13:53:33 +0530

    First commit - onDev

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,33 @@
+import styles from "../style";
+import { arrowUp } from "../assets";
+
+const GetStarted = () => {
+  const handleScroll = () => {
+    const featuresSection = document.getElementById("features");
+    if (featuresSection) {
+      featuresSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div
+      onClick={handleScroll}
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    >
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+            <span className="text-gradient">Get</span>
+          </p>
+          <img src={arrowUp} alt="arrow-up" className="w-[23px] h-[23px] object-contain" />
+        </div>
+        
+        <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+          <span className="text-gradient">Started</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GetStarted;
